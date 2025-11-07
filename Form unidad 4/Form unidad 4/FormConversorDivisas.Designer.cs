0d8d6aeb1d92e2f33a5096b45b5f116ccbc281a3
angelmgibbs-blip <angelmgibbs@gmail.com>
2025-11-07 16:02:14 -0400

    ejercicio 2

--- Form unidad 4/Form unidad 4/FormConversorDivisas.Designer.cs
+++ Form unidad 4/Form unidad 4/FormConversorDivisas.Designer.cs
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.cmbMonedaOrigen = new System.Windows.Forms.ComboBox();
+            this.cmbMonedaDestino = new System.Windows.Forms.ComboBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monto a Convertir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "De";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(498, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "A";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(675, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(362, 196);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(35, 57);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(100, 20);
+            this.txtMonto.TabIndex = 5;
+            // 
+            // cmbMonedaOrigen
+            // 
+            this.cmbMonedaOrigen.FormattingEnabled = true;
+            this.cmbMonedaOrigen.Items.AddRange(new object[] {
+            "USD",
+            "EUR",
+            "DOP"});
+            this.cmbMonedaOrigen.Location = new System.Drawing.Point(236, 58);
+            this.cmbMonedaOrigen.Name = "cmbMonedaOrigen";
+            this.cmbMonedaOrigen.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonedaOrigen.TabIndex = 6;
+            // 
+            // cmbMonedaDestino
+            // 
+            this.cmbMonedaDestino.FormattingEnabled = true;
+            this.cmbMonedaDestino.Items.AddRange(new object[] {
+            "USD",
+            "EUR",
+            "DOP"});
+            this.cmbMonedaDestino.Location = new System.Drawing.Point(450, 58);
+            this.cmbMonedaDestino.Name = "cmbMonedaDestino";
+            this.cmbMonedaDestino.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonedaDestino.TabIndex = 7;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(655, 57);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 8;
+            // 
+            // FormConversorDivisas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.cmbMonedaDestino);
+            this.Controls.Add(this.cmbMonedaOrigen);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormConversorDivisas";
             this.Text = "FormConversorDivisas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.ComboBox cmbMonedaOrigen;
+        private System.Windows.Forms.ComboBox cmbMonedaDestino;
+        private System.Windows.Forms.TextBox txtResultado;
     }
 }
--- Form unidad 4/Form unidad 4/FormConversorDivisas.cs
+++ Form unidad 4/Form unidad 4/FormConversorDivisas.cs
@@ -16,5 +16,47 @@ namespace Form_unidad_4
         {
             InitializeComponent();
         }
+        private const double TASA_EUR_A_USD = 1.07; // 1 EUR = 1.07 USD
+        private const double TASA_DOP_A_USD = 0.017; // 1 DOP = 0.017 USD (aprox 1/58)
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtMonto.Text, out double monto) || monto < 0)
+            {
+                MessageBox.Show("Ingrese un monto numérico positivo válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cmbMonedaOrigen.SelectedItem == null || cmbMonedaDestino.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione las monedas de origen y destino.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string origen = cmbMonedaOrigen.SelectedItem.ToString();
+            string destino = cmbMonedaDestino.SelectedItem.ToString();
+            double resultado = 0;
+            double montoEnUSD = ConvertirMonedaABase(origen, monto);
+            if (destino == "USD")
+            {
+                resultado = montoEnUSD;
+            }
+            else if (destino == "EUR")
+            {
+                resultado = montoEnUSD / TASA_EUR_A_USD;
+            }
+            else if (destino == "DOP")
+            {
+                resultado = montoEnUSD / TASA_DOP_A_USD;
+            }
+            txtResultado.Text = resultado.ToString("N2"); // Mostrar con 2 decimales
+        }
+        private double ConvertirMonedaABase(string origen, double monto)
+        {
+            if (origen == "USD") return monto;
+            if (origen == "EUR") return monto * TASA_EUR_A_USD;
+            if (origen == "DOP") return monto * TASA_DOP_A_USD;
+            return 0;
     }
-}
+    }
+    }
