83beb84386022fa3fcdb36a2eab52ac2950ada9e
angelmgibbs-blip <angelmgibbs@gmail.com>
2025-11-07 16:24:30 -0400

    ejercicio 3

--- Form unidad 4/Form unidad 4/FormFibonacci.Designer.cs
+++ Form unidad 4/Form unidad 4/FormFibonacci.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtLimite = new System.Windows.Forms.TextBox();
+            this.txtResultadoFib = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLimite
+            // 
+            this.txtLimite.Location = new System.Drawing.Point(232, 105);
+            this.txtLimite.Name = "txtLimite";
+            this.txtLimite.Size = new System.Drawing.Size(100, 20);
+            this.txtLimite.TabIndex = 0;
+            // 
+            // txtResultadoFib
+            // 
+            this.txtResultadoFib.Location = new System.Drawing.Point(390, 105);
+            this.txtResultadoFib.Name = "txtResultadoFib";
+            this.txtResultadoFib.Size = new System.Drawing.Size(100, 20);
+            this.txtResultadoFib.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Límite";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Serie Generada";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(276, 153);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(146, 23);
+            this.btnGenerar.TabIndex = 4;
+            this.btnGenerar.Text = "Generar Serie";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // FormFibonacci
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultadoFib);
+            this.Controls.Add(this.txtLimite);
+            this.Name = "FormFibonacci";
             this.Text = "FormFibonacci";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtLimite;
+        private System.Windows.Forms.TextBox txtResultadoFib;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerar;
     }
 }
--- Form unidad 4/Form unidad 4/FormFibonacci.cs
+++ Form unidad 4/Form unidad 4/FormFibonacci.cs
@@ -16,5 +16,31 @@ namespace Form_unidad_4
         {
             InitializeComponent();
         }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtLimite.Text, out int limite) || limite <= 0)
+            {
+                MessageBox.Show("Ingrese un número entero positivo válido para el límite.", "Error de Entrada");
+                txtResultadoFib.Clear();
+                return;
+            }
+            int a = 0;
+            int b = 1;
+            System.Text.StringBuilder serie = new System.Text.StringBuilder();
+            
+            if (limite >= 1) serie.Append(a); // El primer término es 0
+
+            if (limite >= 2) serie.Append(", ").Append(b); // El segundo término es 1
+
+            for (int i = 3; i <= limite; i++)
+            {
+                int c = a + b;
+                serie.Append(", ").Append(c);
+                a = b;
+                b = c;
+           }
+            txtResultadoFib.Text = serie.ToString();
+        }
     }
 }
